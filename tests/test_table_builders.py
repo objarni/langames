33e8a8a9447b7f224760862d7f303c0aa6bbbcb6
Olof Bjarnson <olof.bjarnason@gmail.com>
2020-10-01 18:48:26 +0200

    improve expected,actual error messages from pytest

--- tests/test_table_builders.py
+++ tests/test_table_builders.py
@@ -7,8 +7,8 @@ def test_simple_2x2_csv_table():
     builder.add_row(['1', '2'])
     builder.add_row(['3', '4'])
     expected = 'Anna,Kalle\n1,2\n3,4'
-    got = builder.get_csv_text()
-    assert expected == got
+    actual = builder.get_csv_text()
+    assert expected == actual
 
 
 def test_simple_2x2_text_table():
@@ -18,8 +18,8 @@ def test_simple_2x2_text_table():
     builder.add_row(['3', '4'])
     # TODO: Fill in expected string below!
     expected = ''
-    got = builder.get_string_output()
-    assert expected == got
+    actual = builder.get_string_output()
+    assert expected == actual
 
 
 def test_simple_2x2_html_table():
@@ -34,5 +34,5 @@ def test_simple_2x2_html_table():
   <tr><td>3</td><td>4</td></tr>
 </table>
 '''
-    got = builder.get_html()
-    assert expected == got
+    actual = builder.get_html()
+    assert expected == actual
